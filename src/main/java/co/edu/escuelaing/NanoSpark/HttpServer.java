51c21c23578654592be6966aef4567de2ca1e0f7
Johann Bogotá <johann.bogota@mail.escuelaing.edu.co>
2021-02-11 20:45:24 -0500

    Archivos basicos

--- src/main/java/co/edu/escuelaing/NanoSpark/HttpServer.java
+++ src/main/java/co/edu/escuelaing/NanoSpark/HttpServer.java
@@ -2,16 +2,12 @@ package co.edu.escuelaing.NanoSpark;
 
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
