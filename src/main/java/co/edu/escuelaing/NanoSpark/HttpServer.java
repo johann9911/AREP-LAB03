175c492a2a994a5f5249481d8b917722fc97989d
Johann Bogotá <johann.bogota@mail.escuelaing.edu.co>
2021-02-11 21:04:18 -0500

    Corrección

--- src/main/java/co/edu/escuelaing/NanoSpark/HttpServer.java
+++ src/main/java/co/edu/escuelaing/NanoSpark/HttpServer.java
@@ -125,7 +125,7 @@ public class HttpServer {
 		if (System.getenv("PORT") != null) {
 			return Integer.parseInt(System.getenv("PORT"));
 		}
-		return 36000; // returns default port if heroku-port isn't set(i.e. on localhost)
+		return 35000; // returns default port if heroku-port isn't set(i.e. on localhost)
 	}
 	
 }
